bd280169f689c41078533445c70f5b645c436017
dairiskk <dairis.klesniks@gmail.com>
2024-12-28 14:46:16 +0200

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
 import { UserModule } from './user/user.module';
+import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [UserModule, UserModule],
+  imports: [UserModule, UserModule, ProjectModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PrismaService } from '../prisma.service';
 import { UserController } from './user.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [forwardRef(() => AuthModule)],
   providers: [UserService, PrismaService],
   controllers: [UserController],
+  exports: [UserService], // Export UserService to be used in other modules
 })
 export class UserModule {}
